18a81cdbd32a9308b08e3e7eee5d69bf00d43024
GitHub <noreply@github.com>
2023-06-22 09:04:11 +0000

    update (automated)

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,1 @@
-export default function isDisposable(email:string,{remote}?:{remote?:boolean|undefined}):Promise<boolean>
+export default function isDisposable(email:string,{remote}?:{remote?:boolean|undefined},):Promise<boolean>
